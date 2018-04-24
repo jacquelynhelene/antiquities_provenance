1eaca40bc5bcf316112ac980852996ff5ff295ae
Jacquelyn Helene Clements <jacquelynhelene@gmail.com>
2018-04-24 10:04:26 -0700

    Modified Knoedler selected info and added another make_report option.

--- knoedler_antiquities.R
+++ knoedler_antiquities.R
@@ -3,28 +3,31 @@ library(GPIdata2)
 
 
 # Load google sheet from Knoedler
-
 library(googlesheets)
 
-knoedler_antiquities <- gs_url("https://docs.google.com/spreadsheets/d/1e7uf3WVorv94ypdj9ymnlXeReecPBOqn8sQmoxnP758/edit#gid=0")
+knoedler_antiquities <- gs_url("https://docs.google.com/spreadsheets/d/1e7uf3WVorv94ypdj9ymnlXeReecPBOqn8sQmoxnP758/edit# gid=0")
 knoedler_antiquities_data <- gs_read(knoedler_antiquities)
 
+# Load all Knoedler
 raw_load("raw_knoedler")
 
-# All Knoedler
-
+# Join google sheet to Knoedler
 ancient_knoedler_all <- knoedler_antiquities_data %>%
   left_join(raw_knoedler, by = c("knoedler_number" = "knoedler_number"))
 
 
 # Selected Fields ----
-knoedler_info <- raw_knoedler %>%
-  select(pi_record_no, stock_book_no, knoedler_number, art_authority_1, title, description, subject, object_type, materials) 
+knoedler_info_select <- raw_knoedler %>%
+  select(pi_record_no, stock_book_no, knoedler_number, art_authority_1, title, object_type, materials, seller_name_1, buyer_name_1, entry_date_year, sale_date_year) 
 
 ancient_knoedler <- knoedler_antiquities_data %>%
-  left_join(knoedler_info, by = c("knoedler_number" = "knoedler_number"))
+  left_join(knoedler_info_select, by = c("knoedler_number" = "knoedler_number"))
+
 
 make_report(ancient_knoedler)
+
+make_report(ancient_knoedler_all)
+
 
 # Digging and Dealing
 
